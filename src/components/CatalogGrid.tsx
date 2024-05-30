34dcf01d7a473e865cc4acec61fe7c069bc545d0
Elias Ablan <eliasablan93@gmail.com>
2024-05-30 17:30:04 -0400

    refactor catalog and its components

---
+++ src/components/CatalogGrid.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Product } from '@/lib/shopify/types'
+import ProductCard from '@/components/ProductCard'
+
+export default function CatalogGrid({products }: {products: Product[]}) {
+  if (products.length === 0) return null
+  return (
+    <ul className="aspect-square grid-cols-1 transition-opacity sm:grid-cols-2 lg:grid-cols-3">
+      {products.map((product) => (
+        <li
+          key={product.handle}
+          className="aspect-square animate-fadeIn transition-opacity"
+        >
+          <ProductCard
+            className="inline-block"
+            product={product}
+            src={product.featuredImage?.url}
+            alt={product.title}
+            sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
