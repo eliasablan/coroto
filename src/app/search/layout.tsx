4cc8875c797f21ab5d6d249bab3217b815a07710
Elias Ablan <eliasablan93@gmail.com>
2024-06-06 17:07:44 -0400

    add Suspense fallback skeleton to catalog search bar

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { getCollections } from '@/lib/shopify'
 import ScrollButton from '@/components/ScrollButton'
 import SearchBar from '@/components/SearchBar'
 import CatalogSorter from '@/components/CatalogSorter'
 import CollectionFilter from '@/components/CollectionFilter'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export default async function SearchLayout({
   children,
@@ -15,7 +16,15 @@ export default async function SearchLayout({
   return (
     <div className="container grid grid-cols-1 gap-4 md:grid-cols-2">
       <div className="sticky top-[51px] z-50 col-span-1 bg-background py-4 md:col-span-2">
-        <SearchBar className="sticky top-[83px] z-50 flex items-center" />
+        <Suspense
+          fallback={
+            <div className="sticky top-[83px] z-50 flex items-center">
+              <Skeleton className="h-10 w-full rounded-md" />
+            </div>
+          }
+        >
+          <SearchBar className="sticky top-[83px] z-50 flex items-center" />
+        </Suspense>
       </div>
       <CollectionFilter collections={collections} />
       <CatalogSorter />
