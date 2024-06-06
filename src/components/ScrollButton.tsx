adf2d0b030457111a0db815b5ea88a0141e582f1
Elias Ablan <eliasablan93@gmail.com>
2024-06-06 14:42:22 -0400

    scroll to top button refactor

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button } from './ui/button'
 import { ArrowUp } from 'lucide-react'
 import {
@@ -9,14 +9,35 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip'
+import { cn } from '@/lib/utils'
 
 export default function ScrollButton() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY
+      setIsVisible(scrollY > 0) // Show button only when scrolled down
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll) // Cleanup
+  }, [])
+
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
           <Button
-            className="fixed bottom-8 left-1/2 -translate-x-5 transition-transform hover:scale-105"
+            // className="fixed bottom-8 left-1/2 -translate-x-5 transition-transform hover:scale-105"
+            // className={`fixed bottom-8 left-1/2 -translate-x-5 transition-transform hover:scale-105 ${
+            //   isVisible ? 'translate-y-0' : 'translate-y-full' // Slide-in animation
+            // }`}
+            className={cn(
+              'fixed bottom-8 right-8 transition-transform hover:scale-105 sm:left-1/2 sm:right-0 sm:-translate-x-5',
+              isVisible ? 'translate-y-0' : 'translate-y-20' // Slide-in animation
+            )}
             size="icon"
             onClick={() => scrollToTop()}
           >
