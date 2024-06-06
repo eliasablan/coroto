8c23e092215db50b79c37d188599c359117f71df
Elias Ablan <eliasablan93@gmail.com>
2024-06-06 15:04:12 -0400

    docker build errors fix

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { getCollections } from '@/lib/shopify'
 import ScrollButton from '@/components/ScrollButton'
 import SearchBar from '@/components/SearchBar'
